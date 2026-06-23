cf5fcfae71463facef4310494ac9fb674e6b3af8
JOZET7285 <JOZET7285o@gmail.com>
2026-06-22 20:14:36 -0600

    Archivos corregidos evitando errores en hyprland.lua

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,9 +1,5 @@
-require("lua.types")
----@type HyprlandAPI
-local hl = _G.hl or error("Hyprland API 'hl' no disponible")
-
-require("lua.inicializar")
-require("lua.monitores")
-require("lua.combinaciones")
+require("lua/inicializar")
+require("lua/monitores")
+require("lua/combinaciones")
 
 hl.general({ border_size = 2 })
