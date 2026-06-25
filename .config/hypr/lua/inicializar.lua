4576c1b0272111679511e7107a23d52f7028db99
JOZET7285 <JOZET7285o@gmail.com>
2026-06-25 13:52:07 -0600

    Mejoras del waybar + implementacion de quickshell en el rice

--- .config/hypr/lua/inicializar.lua
+++ .config/hypr/lua/inicializar.lua
@@ -1,5 +1,15 @@
-hl.dsp.exec_cmd("waybar")
-hl.dsp.exec_cmd("quickshell")
+local function run_if_not_running(process_name, command)
+    local handle = io.popen("pgrep -x " .. process_name)
+    local result = handle:read("*a")
+    handle:close()
+
+    if result == "" then
+        hl.dsp.exec_cmd(command)
+    end
+end
+
+run_if_not_running("quickshell", "quickshell -s ~/.config/quickshell/main.qml")
+run_if_not_running("waybar", "waybar")
 hl.dsp.exec_cmd("hyprpaper")
 hl.dsp.exec_cmd("nm-applet --indicator")
 hl.dsp.exec_cmd("pipewire")
