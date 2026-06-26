8bf33f0e7cddf85934e5c8e14d0d36035667726f
JOZET7285 <JOZET7285o@gmail.com>
2026-06-26 09:26:31 -0600

    Logica estructurada del Main y shell de quichshell. Prueba 1.3: EnergyWidget

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -4,5 +4,11 @@ dofile(config_dir .. "lua/monitores.lua")
 dofile(config_dir .. "lua/combinaciones.lua")
 dofile(config_dir .. "lua/window_rules.lua")
 
+hl.config({
+    input = {
+        kb_layout = "latam",
+    }
+})
+
 gaps_in = 2
 gaps_out = 2
