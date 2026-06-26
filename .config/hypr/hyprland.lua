c05d00b7fce7de0d86ec9e99ead2a317d719abcf
JOZET7285 <JOZET7285o@gmail.com>
2026-06-26 09:22:19 -0600

    Logica estructurada del Main y shell de quichshell. Prueba 1: EnergyWidget

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,7 +1,8 @@
-require("lua/inicializar")
-require("lua/monitores")
-require("lua/combinaciones")
-require("lua/window_rules")
+local config_dir = os.getenv("HOME") .. "/.config/hypr/"
+dofile(config_dir .. "lua/inicializar.lua")
+dofile(config_dir .. "lua/monitores.lua")
+dofile(config_dir .. "lua/combinaciones.lua")
+dofile(config_dir .. "lua/window_rules.lua")
 
 gaps_in = 2
 gaps_out = 2
