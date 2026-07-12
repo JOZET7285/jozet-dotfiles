fedabd4aa50b2dc06e7fa59482353811e61b401e
JOZET <150647952+JOZET7285@users.noreply.github.com>
2026-07-12 12:58:01 -0600

    Delete backend/build directory

--- backend/build/CMakeFiles/JozetPluginplugin.dir/JozetPluginplugin_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/JozetPluginplugin.dir/JozetPluginplugin_autogen/mocs_compilation.cpp.o: \
- /home/pruebas/jozet-dotfiles/backend/build/JozetPluginplugin_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
