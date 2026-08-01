cebfb30cb62c41d940dccad468137c58ef5e9f27
JOZET <150647952+JOZET7285@users.noreply.github.com>
2026-07-31 22:28:10 -0600

    Update file path for matugen-colors.json

--- backend/System/Readers/MatugenReader.cpp
+++ backend/System/Readers/MatugenReader.cpp
@@ -7,7 +7,7 @@
 namespace jozet {
 
 MatugenReader::MatugenReader(QObject *parent) : QObject(parent) {
-    m_filePath = QDir::homePath() + "/.local/share/jozet/matugen-colors.json";
+    m_filePath = QDir::homePath() + "/.local/share/jzt/matugen-colors.json";
     load();
 
     QDir().mkpath(QFileInfo(m_filePath).absolutePath());
