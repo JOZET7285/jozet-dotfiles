85deda2e2ee907143673686a461201d5d04e4fb1
jozet7285 <jozet7285@gmail.com>
2026-08-04 18:24:43 -0600

    datos.lua Fixed

--- .config/hypr/lua/inicializar.lua
+++ .config/hypr/lua/inicializar.lua
@@ -5,3 +5,19 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("systemctl --user restart pipewire pipewire-pulse wireplumber")
     hl.exec_cmd("hypridle")
 end)
+
+package.path = package.path .. ";" .. os.getenv("HOME") .. "/.local/share/jzt/?.lua"
+
+local ok, datos = pcall(require, "datos")
+if not ok then
+    datos = {
+        gaps_in = 5,
+        gaps_out = 15,
+        border_radius = 8,
+        border_size = 2,
+        kb_layout = "latam",
+        cursor_theme = "Bibata-Modern-Ice",
+        cursor_size = 24,
+        cursor_speed = 0.0,
+    }
+end
